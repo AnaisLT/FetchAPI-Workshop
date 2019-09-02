ec35564d635e175bf85c300d8ae47ff24956c96b
AnaisLT <trope.anais@gmail.com>
2019-09-02 12:02:26 +0100

    Example using JSONPlaceholder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      error: null,
+    }
+  }
+
+  fetchUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then(res => res.json())
+    .then(data => this.setState({ users: data, isLoading: false }))
+    .catch(error => this.setSate({ error }));
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div>
+        <h1>Random Users</h1>
+        {users.map((user) => {
+          const { id, name, username, email } = user
+          return <div key={id}>
+            <h2>Name: {name}</h2>
+            <p>Username: {username}</p>
+            <p>Email: {email}</p> 
+          </div>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
